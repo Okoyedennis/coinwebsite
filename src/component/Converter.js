f75feaaa9868d8ae20d10d4c967da0fbc98ad74a
Dennis <okoyedennis7@gmail.com>
2021-08-16 09:07:26 +0100

    first update

--- src/component/Converter.js
+++ src/component/Converter.js
@@ -4,86 +4,70 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 
 const Converter = () => {
-  const [text1, setText1] = useState(1);
-  const [text2, setText2] = useState();
-  const [country1, setCountry1] = useState([]);
-  const [country2, setCountry2] = useState([]);
-  const [value1, setValue1] = useState(1);
-  const [value2, setValue2] = useState(1);
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("");
+  const [rate, setRate] = useState([]);
 
   useEffect(() => {
-    getApi();
+    getRate();
   }, []);
 
-  const getApi = async () => {
-    const result = await axios.get(
-      "http://data.fixer.io/api/latest?access_key=4b6fdaa6b09b588b789709447d2370a7"
-    );
-    console.log(result.data.rates);
-    setCountry1(result.data.rates);
-    setCountry2(result.data.rates);
+  const getRate = (first, second) => {
+    axios({
+      method: "GET",
+      url: `https://free.currconv.com/api/v7/convert?q=${first}_${second}&compact=ultra&apiKey=afcd6d2dd333f78493e9`,
+    })
+      .then((response) => {
+        console.log(response.data);
+
+        setRate(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const convert = (e) => {
     e.preventDefault();
-    let num = (value2 / value1) * text1;
-    setText2(num.toFixed(2));
+    if (!first && !second) {
+      alert("Please Enter Currency");
+    } else {
+      getRate(first, second);
+    }
   };
   return (
     <Container maxW="container.lg">
       <form onSubmit={convert}>
         <div className="converter">
-          <div className="input">
-            <Text style={{ fontSize: "20px", fontWeight: "500" }} mb="8px">
-              Amount
-            </Text>
-            <Input
-              placeholder="Enter Amount"
-              className="input"
-              value={text1 || " "}
-              onChange={(e) => setText1(e.target.value)}
-              type="text"
-              size="md"
-            />
-          </div>
-          <div className="space" />
           <div className="selectContry1">
             <Text style={{ fontSize: "20px", fontWeight: "500" }} mb="8px">
               From:
             </Text>
-            <Select
-              className="select"
-              placeholder="Select option"
-              onChange={(e) => setValue1(e.target.value)}
-            >
-              {Object.keys(country1).map((value, index) => (
-                <option key={index} value={country1[value]}>
-                  {value}
-                </option>
-              ))}
-            </Select>
+            <Input
+              type="text"
+              value={first}
+              onChange={(e) => setFirst(e.target.value)}
+              placeholder="Enter Currency AUD, USD, EUR etc"
+            />
           </div>
           <div className="space" />
           <div className="selectContry2">
             <Text style={{ fontSize: "20px", fontWeight: "500" }} mb="8px">
               To:
             </Text>
-            <Select
-              className="select"
-              placeholder="Select option"
-              onChange={(e) => setValue2(e.target.value)}
-            >
-              {Object.keys(country2).map((value, index) => (
-                <option key={index} value={country1[value]}>
-                  {value}
-                </option>
-              ))}
-            </Select>
+            <Input
+              type="text"
+              value={second}
+              onChange={(e) => setSecond(e.target.value)}
+              placeholder="Enter Currency AUD, USD, EUR etc"
+            />
           </div>
         </div>
         <div className="buttomRow">
           <div className="result">
-            <h1>{text2}</h1>
+            <h1>
+              1 {first} = {rate[`${first}_${second}`]} {second}
+            </h1>
           </div>
           <Button
             type="submit"
